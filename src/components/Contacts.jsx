48187ceee1323bc493375e24d77599c0963b8e22
DN200194DEO <yevhenii.dychenko@privatbank.ua>
2024-05-23 19:33:25 +0300

    create site card Ivan Bereza

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -10,28 +10,28 @@ function Contacts(props) {
         <div className={classes.container}>
             <div>
                 <div className={classes.title}>
-                    Andrii
+                    Ivan Bereza
                 </div>
                 <div className={classes.description}>
-                    <span className={classes.bold}>Head</span> of VIP <span className={classes.bold}>customer service department</span>
+                    <span className={classes.bold}>Georgian</span> olive <span className={classes.bold}>platator</span>
                 </div>
             </div>
             <div className={classes.holder}>
-                <a style={{animationDuration: '.6s'}} className={classes.icon} href="https://wa.me/995599427509">
-                    <AiOutlineWhatsApp title="Whatsapp" />
-                </a>
-                <a style={{animationDuration: '.9s'}} className={classes.icon} href="https://t.me/BioAndrii">
+                {/*<a style={{animationDuration: '.6s'}} className={classes.icon} href="https://wa.me/995599427509">*/}
+                {/*    <AiOutlineWhatsApp title="Whatsapp" />*/}
+                {/*</a>*/}
+                <a style={{animationDuration: '.9s'}} className={classes.icon} href="https://t.me/lat424532">
                     <TbBrandTelegram title="Telegram" />
                 </a>
-                <a style={{animationDuration: '1.2s'}} className={classes.icon} href="viber://chat?number=+995599427509">
-                    <FaViber title="Viber" />
-                </a>
-                <a style={{animationDuration: '1.5s'}} className={classes.icon} href="mailto:Bio.cottage.gr@gmail.com?subject=&body=">
+                {/*<a style={{animationDuration: '1.2s'}} className={classes.icon} href="viber://chat?number=+995599427509">*/}
+                {/*    <FaViber title="Viber" />*/}
+                {/*</a>*/}
+                <a style={{animationDuration: '1.5s'}} className={classes.icon} href="mailto:Latifundistge@gmail.com?subject=&body=">
                     <HiOutlineMail title="Mail" />
                 </a>
-                <a style={{animationDuration: '1.8s'}} className={classes.icon} href="tel:+995599427509">
-                    <HiOutlinePhone title="Phone" />
-                </a>
+                {/*<a style={{animationDuration: '1.8s'}} className={classes.icon} href="tel:+995599427509">*/}
+                {/*    <HiOutlinePhone title="Phone" />*/}
+                {/*</a>*/}
             </div>
         </div>
     );
