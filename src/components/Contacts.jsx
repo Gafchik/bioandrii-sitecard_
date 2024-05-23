de79a2ff4a18483f7b9017234da35d0d677cf59c
DN200194DEO <yevhenii.dychenko@privatbank.ua>
2024-05-23 20:36:37 +0300

    create site card  Bereza

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -13,7 +13,7 @@ function Contacts(props) {
                     Ivan Bereza
                 </div>
                 <div className={classes.description}>
-                    <span className={classes.bold}>Georgian</span> olive <span className={classes.bold}>platator</span>
+                    <span className={classes.bold}>Georgia olive platator</span>
                 </div>
             </div>
             <div className={classes.holder}>
